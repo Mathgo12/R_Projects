73ca4943d95510a995d56a0af3b2f396ffb435a0
Mathgo12 <sur123app@gmail.com>
2022-03-27 23:57:32 -0700

    autocorrelation and moving average processes

--- time_series.r
+++ time_series.r
@@ -1,1 +1,42 @@
-print('This is to make sure that Github is really working')
+
+vect.1 <- rnorm(2)
+vect.2 <- c(3,4)
+vect.3 <- c(1,2)
+
+cat("First operation: ", vect.1 + vect.2, "\n")
+cat("Second operation: ", vect.2 + vect.3, "\n")
+
+#Modulo operator
+num1 <- 12
+num2 <- 5
+
+print(num1%%num2) # prints 2
+
+x=NULL
+x[1]=0
+for(i in 2:200){
+  x[i] = x[i-1] + rnorm(1,0,5)
+  
+}
+
+random_walk = diff(ts(x))
+print(length(random_walk))
+
+## Moving Average Processes - order 3
+
+noise = rnorm(10000)
+mov_avg3 = NULL
+for(i in 4:10000){
+  mov_avg3[i] = noise[i] + 0.7*noise[i-1] + 0.2*noise[i-2] + 0.1*noise[i-3]
+  
+}
+
+moving_average_process = ts(mov_avg3[4:10000])
+par(mfrow=c(2,1)) # multi frame plots - 2 columns and 1 row
+plot(moving_average_process)
+acf(moving_average_process)
+
+
+
+
+
